b2268a33b4fee8795238b35ae831ed906d18360f
Otávio Henrique <otaviocap@hotmail.com.br>
2023-08-17 23:36:44 -0300

    Update

--- _nuxt/DocumentDrivenNotFound.362bce2c.js
+++
@@ -1,1 +0,0 @@
-import{g as n,B as e}from"./entry.debfde12.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
---
+++ _nuxt/DocumentDrivenNotFound.e2e49579.js
@@ -0,0 +1,1 @@
+import{g as n,B as e}from"./entry.d2c05059.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
